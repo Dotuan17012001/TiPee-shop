7930f1fd7215700d047e180f5655c437a835a019
Dotuan17012001 <dovantuan17012001@gmail.com>
2023-09-29 07:40:49 +0700

    setup-route

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import LoginPage from './pages/login';
+import ContactPage from './pages/contact';
+import BookPage from './pages/book';
+import { Outlet } from "react-router-dom";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home';
+
+const Layout = () => {
+  return (
+    <div className='layout-app'>
+      <Header/>
+      <Outlet/>
+      <Footer/>
+    </div>
+  )
+}
+export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout/>,
+      errorElement: <div>Sorry!!! 404 not found</div>,
+      children: [
+        { index:true, element: <Home/>}
+        ,
+        {
+          path: "contacts",
+          element: <ContactPage />,
+        },
+        {
+          path: "book",
+          element: <BookPage />,
+        },
+      ],
+    },
+
+    {
+      path: "/login",
+      element: <LoginPage/>,
+    },
+  ]);
+      return (
+        <>
+          <RouterProvider router={router} />
+        </>
+      )
+}
---
+++ src/pages/book/index.jsx
@@ -0,0 +1,7 @@
+const BookPage = () => {
+    return ( 
+        <>Book page</>
+     );
+}
+ 
+export default BookPage;
